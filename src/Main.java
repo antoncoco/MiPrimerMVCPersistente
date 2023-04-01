7e95f233c6670d0de9fbce45c885c715b3a6383c
antoncoco <cocoletziantonio@gmail.com>
2023-04-01 11:55:14 -0600

    Global(delete): Eliminar redundancias Se han eliminado archivos que son duplicados ligeramente modificados de otro y también se han eliminado comentarios en la clase Main. Ref#05c2420 Reviewed-by: LACC

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
-//import controlador.ControladorEspecialidad;
 import controlador.ControladorEspecialidadGUI;
 import modelo.ModeloEspecialidad;
-//import vista.VistaEspecialidad;
 import vista.VistaEspecialidadGUI;
 
 public class Main {
     public static void main(String[] args) {
-        /*ModeloEspecialidad mp = new ModeloEspecialidad();
-        VistaEspecialidad vp  = new VistaEspecialidad();
-        ControladorEspecialidad cp = new ControladorEspecialidad(mp,vp);
-        cp.iniciarVistaEspecialidad();*/
-
         ModeloEspecialidad mp = new ModeloEspecialidad();
         VistaEspecialidadGUI vp = new VistaEspecialidadGUI();
         ControladorEspecialidadGUI cp = new ControladorEspecialidadGUI(mp,vp);
